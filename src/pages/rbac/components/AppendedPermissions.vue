a73315090225072d39c2feb7ce15c3c38c161418
Ilxomiddinov Diyorbek <ilxomiddinovdiyorbek1@gmail.com>
2025-10-17 16:44:21 +0500

    change

--- src/pages/rbac/components/AppendedPermissions.vue
+++ src/pages/rbac/components/AppendedPermissions.vue
@@ -1,65 +1,57 @@
 <script lang="ts" setup>
-import { useRbacRoleStore } from '@/stores/rbac'
-import { onMounted, ref } from 'vue'
-import { useToast } from 'vue-toastification'
+import { useRbacRoleStore } from "@/stores/rbac";
+import { onMounted, ref } from "vue";
+import { useToast } from "vue-toastification";
 
-const rbacRulesStore = useRbacRoleStore()
+const rbacRulesStore = useRbacRoleStore();
 
-const { permissions, permissionsById } = storeToRefs(rbacRulesStore)
+const { permissions, permissionsById } = storeToRefs(rbacRulesStore);
 
-const toast = useToast()
+const toast = useToast();
 
-const rolePermissionId = ref<number>(1)
+const rolePermissionId = ref<number>(1);
 
 async function appendPermissions() {
   rbacRulesStore.permissions.forEach(permission => {
-    permissionsById.value.permissions.forEach(pById => {
-      if (permission.id === pById.id)
-        permission.isPermitted = true
-    })
-
-  })
+    permissionsById.value.forEach(pById => {
+      if (permission.id === pById.id) permission.isPermitted = true;
+    });
+  });
 }
 
 async function changePermissions() {
-  const append = []
+  const append = [];
 
   permissions.value.forEach(permission => {
     if (permission.isPermitted) {
-      append.push(
-        permission.id,
-      )
+      append.push(permission.id);
     }
-  })
+  });
 
-  const result = await rbacRulesStore.changePermissions(append, rolePermissionId.value)
+  const result = await rbacRulesStore.changePermissions(
+    append,
+    rolePermissionId.value
+  );
   if (result)
-    toast.success('Menyu ruxsatnomalari muvaffaqiyatli o\'zgartirildi')
+    toast.success("Menyu ruxsatnomalari muvaffaqiyatli o'zgartirildi");
 }
 
 async function getRolePermissions(id) {
-  await rbacRulesStore.fetchRbacPermissionsById(id)
-  await appendPermissions()
+  await rbacRulesStore.fetchRbacPermissionsById(id);
+  await appendPermissions();
 }
 
 onMounted(() => {
-  getRolePermissions(rolePermissionId.value)
-})
+  getRolePermissions(rolePermissionId.value);
+});
 </script>
 
 <template>
   <VCard flat>
     <VCardActions>
       <VRow>
-        <VCol
-          cols="12"
-          md="4"
-        />
-        <VCol
-          class="mt-3"
-          cols="12"
-          md="8"
-        >
+        <VCol cols="12" md="4" />
+        <VCol class="mt-3" cols="12" md="8">
           <VAutocomplete
             v-model="rolePermissionId"
             :items="rbacRulesStore.roles"
@@ -91,10 +83,7 @@ onMounted(() => {
 
     <VCardActions>
       <VSpacer />
-      <VBtn
-        color="primary"
-        @click="changePermissions"
-      >
+      <VBtn color="primary" @click="changePermissions">
         change permissions
       </VBtn>
     </VCardActions>
